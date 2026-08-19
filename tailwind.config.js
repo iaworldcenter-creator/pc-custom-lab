8b643fce01fade4f175ebabb9b561dcf90111ff2
iaworldcenter-creator <iaworldcenter@gmail.com>
2026-08-18 21:16:34 -0600

    Checkout: imagenes 150px contenidas, paso de pago visible, folio OXXO 14 digitos, SEO optimizado

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./checkout.html",
+    "./index.html",
+    "./*.html"
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
