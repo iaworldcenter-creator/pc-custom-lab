b2c1448033af813e0e9836b1a4d7e74c92bfbfb1
iaworldcenter-creator <iaworldcenter@gmail.com>
2026-08-21 23:53:51 -0600

    chore: eliminar script temporal inspect_grid_col

--- inspect_grid_col.py
+++
@@ -1,6 +0,0 @@
-with open(r'E:\sitios web\pc-custom-lab\index.html', 'r', encoding='utf-8') as f:
-    text = f.read()
-
-pos_aside = text.find('</aside>')
-print("=== HTML right after </aside> ===")
-print(text[pos_aside:pos_aside+1000])
